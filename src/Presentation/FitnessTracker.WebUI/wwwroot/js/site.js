a7aacbfaa487aad110c11981e313dce9f5a44ad2
donatgosalcii <gosalci38@gmail.com>
2025-05-15 01:25:54 +0200

    Updated the myprofile page, also intoduced darkmode throughout the website!

--- src/Presentation/FitnessTracker.WebUI/wwwroot/js/site.js
+++ src/Presentation/FitnessTracker.WebUI/wwwroot/js/site.js
@@ -1,4 +1,41 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = themeToggle.querySelector('i');
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
 
-// Write your JavaScript code.
+function setTheme(theme) {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+    
+    if (theme === 'dark') {
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun');
+    } else {
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon');
+    }
+}
+
+function initializeTheme() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        setTheme(savedTheme);
+    } else if (prefersDarkScheme.matches) {
+        setTheme('dark');
+    } else {
+        setTheme('light');
+    }
+}
+
+themeToggle.addEventListener('click', () => {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    setTheme(currentTheme === 'dark' ? 'light' : 'dark');
+});
+
+prefersDarkScheme.addEventListener('change', (e) => {
+    const savedTheme = localStorage.getItem('theme');
+    if (!savedTheme) {
+        setTheme(e.matches ? 'dark' : 'light');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', initializeTheme);
